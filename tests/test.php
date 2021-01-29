1d382eae9bb995212c959c068cf2fbb7bfad0984
confusedCoyote <54775892+confusedCoyote@users.noreply.github.com>
2021-01-29 14:41:00 +0000

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -1,10 +1,11 @@
 <?php
+// Turn off all error reporting
+//error_reporting(0);
+
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 
 use DogTest\DogApi;
 
 $dogCeo = new DogApi();
 
-print_r($dogCeo->allBreeds());
-//echo $dogCeo->random();
-//print_r( $dogCeo->byBreed('gfdgsdfgd'));
+print_r($dogCeo->byBreed("Bulldog"));
