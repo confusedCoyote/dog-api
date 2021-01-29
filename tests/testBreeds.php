618f5e923847b167a1071eb4387be0a2446efff0
confusedCoyote <54775892+confusedCoyote@users.noreply.github.com>
2021-01-29 19:06:20 +0000

    add tests

---
+++ tests/testBreeds.php
@@ -0,0 +1,20 @@
+<?php
+
+use DogTest\DogApi;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Description of testBreeds
+ *
+ * @author Coyote
+ */
+class testBreeds extends TestCase {
+
+    public function testAllBreedsReturns() {
+        $dogCeo = new DogApi();
+        $data = json_decode($dogCeo->allBreeds(), true);
+        // May fail if breeds change
+        $this->assertArrayHasKey('affenpinscher', $data);
+    }
+}
